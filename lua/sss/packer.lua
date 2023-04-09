bb1fab552d843809d53821ca9bc4ef5f0c74f840
sss <rabiasofiane01@gmail.com>
2023-04-09 14:10:22 -0400

    use lsp-zero branch 1.x to fix error

--- lua/sss/packer.lua
+++ lua/sss/packer.lua
@@ -2,7 +2,6 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
-	
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -27,26 +26,27 @@ return require('packer').startup(function(use)
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
 
-	use {
+  use {
 	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
+	  branch = 'v1.x',
 	  requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {                                      -- Optional
-	      'williamboman/mason.nvim',
-	      run = function()
-		pcall(vim.cmd, 'MasonUpdate')
-	      end,
-	    },
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},     -- Required
-	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
 	  }
-	}
-
+  }
 end)
 
