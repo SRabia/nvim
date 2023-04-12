8406180fcf5df901dfd800891284b49181eeab69
sss <rabiasofiane01@gmail.com>
2023-04-12 11:41:28 -0400

    lualine path relative

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,10 +1,17 @@
 require('lualine').setup {
     options = {
-        theme = 'onedark',
+        theme = 'auto',
         icons_enabled = false,
         component_separators = { left = '|', right = '|'},
         section_separators = { left = '', right = ''},
     },
+    sections = {
+        lualine_c = { {'filename', path=1, file_status = true } },
+    },
+    inactive_sections = {
+        lualine_c = { {'filename', path=1, file_status = true } },
+    }
+
 }
 
 require('lualine').setup()
