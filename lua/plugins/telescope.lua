668c659b621748b8d231eec14a773dfd7e117289
sss <rabiasofiane01@gmail.com>
2024-06-17 13:52:44 -0400

    add sf telescope for show hidden file fixed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -116,6 +116,10 @@ return {
 			vim.keymap.set("n", "<leader>sn", function()
 				builtin.find_files({ cwd = vim.fn.stdpath("config") })
 			end, { desc = "[S]earch [N]eovim files" })
+
+			vim.keymap.set("n", "<leader>sf", function()
+				builtin.find_files({ hidden = true })
+			end, { desc = "[S]earch hidden [F]iles" })
 		end,
 	},
 }
